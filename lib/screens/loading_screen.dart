a869fdaab94913abc2d97ee046fcc23521b9b2fa
hisnameispum <sirasitxp@gmail.com>
2020-08-15 22:03:29 -0700

    Added Permissions on Android and iOS

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -19,6 +19,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
         child: RaisedButton(
           onPressed: () {
             //Get the current location
+            getLocation();
           },
           child: Text('Get Location'),
         ),
