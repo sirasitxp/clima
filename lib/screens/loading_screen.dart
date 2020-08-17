1e642108d53bb677801d92d6f83f1d5bf1ce3e92
hisnameispum <sirasitxp@gmail.com>
2020-08-17 01:08:34 -0700

    Added getData() to fetch data from WebForecasting API

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
-
+import 'package:http/http.dart';
 class LoadingScreen extends StatefulWidget {
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  
+  void getData() async{
+    Response response =  await get('https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=439d4b804bc8187953eb36d2a8c26a02');
+    print(response);
+  }
 
   @override
   void initState() {
