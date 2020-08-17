122d26b1fe7b15f87c6170a93ccdc3e0c8d75a2d
hisnameispum <sirasitxp@gmail.com>
2020-08-17 01:11:57 -0700

    Moved getData to be called in build()

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -10,13 +10,14 @@ class _LoadingScreenState extends State<LoadingScreen> {
   
   void getData() async{
     Response response =  await get('https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=439d4b804bc8187953eb36d2a8c26a02');
-    print(response);
+    print(response.body);
   }
 
   @override
   void initState() {
     super.initState();
     getLocation();
+    getData();
   }
   void getLocation() async{
     Location location = Location();
@@ -24,18 +25,11 @@ class _LoadingScreenState extends State<LoadingScreen> {
     print(location.latitude);
     print(location.longitude);
   }
+
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            //Get the current location
-            getLocation();
-          },
-          child: Text('Get Location'),
-        ),
-      ),
-    );
+    getData();
+    return Scaffold();
   }
 }
