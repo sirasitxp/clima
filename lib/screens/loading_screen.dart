2c0373202def730b4adaf392ccaa121b774be0c9
hisnameispum <sirasitxp@gmail.com>
2020-08-18 01:51:35 -0700

    Removed code repetition.

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -13,9 +13,10 @@ class _LoadingScreenState extends State<LoadingScreen> {
     http.Response response =  await http.get('https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=439d4b804bc8187953eb36d2a8c26a02');
     if(response.statusCode == 200){
       String data = response.body;
-      var temp = converter.jsonDecode(data)['main']['temp'];
-      var condition = converter.jsonDecode(data)['weather'][0]['main'];
-      var city = converter.jsonDecode(data)['name'];
+      var decodedData = converter.jsonDecode(data);
+      var temp = decodedData['main']['temp'];
+      var condition = decodedData(data)['weather'][0]['main'];
+      var city = decodedData(data)['name'];
     }
     else{
       print(response.statusCode);
