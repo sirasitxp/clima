a8334eb7d3699b759d066682594bfc5c4535c70c
hisnameispum <sirasitxp@gmail.com>
2020-08-19 00:09:41 -0700

    Added API from OpenWeatherMap

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert' as converter;
+
+const apiKey = '7fa13502414bfba5161d2b5274c33dab';
 class LoadingScreen extends StatefulWidget {
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
@@ -10,7 +12,7 @@ class LoadingScreen extends StatefulWidget {
 class _LoadingScreenState extends State<LoadingScreen> {
   
   void getData() async{
-    http.Response response =  await http.get('https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=439d4b804bc8187953eb36d2a8c26a02');
+    http.Response response =  await http.get('https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=$apiKey');
     if(response.statusCode == 200){
       String data = response.body;
       var decodedData = converter.jsonDecode(data);
