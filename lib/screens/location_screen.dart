489ef2a2bb0d85cccfff9c62ee03e99884c51ad1
hisnameispum <sirasitxp@gmail.com>
2020-08-19 16:41:03 -0700

    Added navigator for city_screen.dart

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:clima/utilities/constants.dart';
 import 'package:clima/services/weather.dart';
+import 'package:clima/screens/city_screen.dart';
 
 class LocationScreen extends StatefulWidget {
   LocationScreen({this.locationWeather});
@@ -71,7 +72,13 @@ class _LocationScreenState extends State<LocationScreen> {
                     ),
                   ),
                   FlatButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      Navigator.push(context, MaterialPageRoute(
+                        builder: (context){
+                          return CityScreen();
+                      }
+                      ));
+                    },
                     child: Icon(
                       Icons.location_city,
                       size: 50.0,
