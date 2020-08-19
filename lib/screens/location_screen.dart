0f98012456fe089078e30337f64e80ab68bb356a
hisnameispum <sirasitxp@gmail.com>
2020-08-19 03:44:16 -0700

    Updated temp in Text widget

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -67,7 +67,7 @@ class _LocationScreenState extends State<LocationScreen> {
                 child: Row(
                   children: <Widget>[
                     Text(
-                      '32°',
+                      '$temp°',
                       style: kTempTextStyle,
                     ),
                     Text(
