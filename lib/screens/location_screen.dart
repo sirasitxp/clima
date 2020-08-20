454c33cfd85511a9a3ed5a1fb142f04ee48e9e61
hisnameispum <sirasitxp@gmail.com>
2020-08-19 21:59:06 -0700

    Add functionality to  back button

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -80,7 +80,8 @@ class _LocationScreenState extends State<LocationScreen> {
                       ),
                       );
                       if (typedName != null){
-
+                        var weatherData = await weather.getCityWeather(typedName);
+                        updateUI(weatherData);
                       }
                     },
                     child: Icon(
